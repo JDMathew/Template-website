434709f3956bc2a08bc1aaf28b3925da8672b67d
Justin M <justindavidmathew@gmail.com>
2020-06-15 15:31:22 +0100

    Adding Firebase
    -Added Firebase to the project
    
    -Set up the Config and Utils
    
    -Setup authentication in Utils
    -Added a Google and FB provider
    
    -Passed signInWithGoogle function out of Utils to our button through ...other props
    from the signin component

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import Button from "./../../components/forms/Button";
+import { signInWithGoogle } from "./../../firebase/utils";
 
 import "./styles.scss";
 
 const SignIn = (props) => {
+  async function handleSubmit(e) {
+    e.preventDefault(); //prevent the page reloading when one pushes the signin with button...
+  }
+
   return (
     <div className="signin">
       <div className="wrap">
         <h2>Login</h2>
         <div className="fromWrap">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="socialSignin">
               <div className="row">
-                <Button>Sign in with Google</Button>
+                <Button onClick={signInWithGoogle}>Sign in with Google</Button>
               </div>
             </div>
           </form>
---
+++ src/firebase/utils.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore"; //firebase firestore
+import "firebase/auth"; //firebase auth library
+import { firebaseConfig } from "./config";
+
+firebase.initializeApp(firebaseConfig); //connect to database
+
+// two instances of auth ot use in application:
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+//SignIn Providers (can create custome providers for Google, Facebook, Github...)
+const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+//const FacebookProvider = new firebase.auth.FacebookAuthProvider();
+
+//Setting custome parameters on providers
+GoogleProvider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(GoogleProvider); //signInWithPopup accepts a provider we pass it
